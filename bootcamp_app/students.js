e33ec6d2c4d997b552e1b9647422a2e8ef8e3f14
easydoesit <michael.grills@gmail.com>
2023-01-23 02:15:00 +0000

    add paramatization to queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,7 +1,15 @@
 const { Pool } = require('pg');
 const cohortName = process.argv[2];
-const resultsNum = process.argv[3];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
+const query = `
+SELECT students.id, students.name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -10,13 +18,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${resultsNum || 5};
-`)
+pool.query(query, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-
+const cohortName = process.argv[2];
+const values = [`${cohortName || 'JUL02'}`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,16 +9,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const query = `
 SELECT teachers.name as teacher, cohorts.name as cohort, COUNT(*) AS total_assistances
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(query, values)
   .then(res => {
     res.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
